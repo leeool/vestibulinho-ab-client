fc9eec44c988b4f9239e9e28c4c9616935e25b2a
Fernnandoo <116328071+Fernnandoo@users.noreply.github.com>
2023-09-20 13:37:32 -0300

    Update home.styled.tsx

--- src/pages/Home/home.styled.tsx
+++ src/pages/Home/home.styled.tsx
@@ -1,15 +1,232 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const Container = styled.div`
-color: #fff;
-`
 
-export const Background = styled.div`
-background-color: #CF2E2E;
-position: absolute;
-width: 100%;
-left: 0;
-top: 0;
-height: 90vh;
-z-index: -1;
-`
+export const Top = styled.div`
+    padding: 30px 0 0 50px;
+    height: 100vh;
+    background-color: #CF2E2E;
+`;
+
+export const TopTitle = styled.p`
+    font-size: 50px;
+    font-weight: bold;
+    color: #fff;
+    margin-left: 10vw;
+    margin-top: 15vh;
+    margin-bottom: 0;
+`;
+
+export const TopContent = styled.span`
+    color: #fff;
+    margin-left: 10vw;
+`;
+
+export const Break = styled.div`
+    margin-left: 10vw;
+`;
+
+export const Etec = styled.span`
+    color: #fff;
+    font-weight: 500;
+`;
+
+export const TopInsc = styled.div`
+    background-color: #fff;
+    border-radius: 5px;
+    width: 520px;
+    height: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #FC2E2E;
+    font-size: 25px;
+    font-weight: bold;
+    padding: 15px;
+    margin-top: 20px;
+    margin-left: 10vw;
+`;
+
+export const Date = styled.div`
+    color: #CF2E2E;
+    position: absolute;
+    font-weight: 500;
+    font-size: 24px;
+    text-align: center;
+`;
+
+export const TopEnd = styled.div`
+    color: #fff;
+    text-align: center;
+    margin-top: 25vh;
+    margin-bottom: 50px;
+    font-weight: 500;
+    font-size: 20px;
+`;
+
+export const IconCircle = styled.div`
+    width: 50px;
+    height: 50px;
+    background-color: #fff;
+    border-radius: 50%;
+    position: absolute;
+`;
+export const SmallIconCircle = styled.div`
+    width: 40px;
+    height: 40px;
+    background-color: #fff;
+    border-radius: 50%;
+    position: absolute;
+`;
+export const VerySmallIconCircle = styled.div`
+    width: 30px;
+    height: 30px;
+    background-color: #fff;
+    border-radius: 50%;
+    position: absolute;
+`;
+
+export const LargeIconCircle = styled.div`
+    width: 90px;
+    height: 90px;
+    background-color: #fff;
+    border-radius: 50%;
+    position: absolute;
+`;
+
+export const BigLargeIconCircle = styled.div`
+    width: 300px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 50%;
+    position: absolute;
+`;
+
+export const Blog = styled.div`
+    margin-left: 10vw;
+    margin-top: 5vh;
+    font-size: 52px;
+    font-weight: bold;
+    padding: 30px 0 50px 50px;
+`;
+
+export const BlogContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding-bottom: 20px;
+    border-bottom: solid #dddddd 2px;
+    width: 70vw;
+`;
+
+export const BlogPost = styled.div`
+    background-color: #f5f5f5;
+    border-radius: 8px;
+    border-bottom-left-radius: 0;
+    padding: 15px;
+    height: 30vh;
+    width: 60vw;
+    margin-top: 5vh;
+`;
+
+export const BlogTitle = styled.div`
+    font-size: 24px;
+    font-weight: 500;
+`;
+
+export const BlogContent = styled.div`
+    font-size: 18px;
+    font-weight: 400;
+    margin-top: 2vh;
+`;
+
+export const BlogButton = styled.button`
+    background-color: #fff;
+    color: #CF2E2E;
+    padding: 5px;
+    width: 7vw;
+    height: 5vh;
+    border-radius: 5px;
+    font-size: 16px;
+    font-weight: 550;
+    border: none;
+`;
+
+export const BlogImage = styled.div`
+    background-color: #f5f5f5;
+    border-radius: 8px;
+    box-shadow: -5px -5px #CF2E2E;
+    width: 15vw;
+    height: 30vh;
+    margin-left: 30px;
+    margin-top: 8vh;
+`;
+
+export const Crono = styled.div`
+    height: 50vh;
+    background-color: #fff;
+    color: #CF2E2E;
+    margin-left: 10vw;
+    margin-top: 5vh;
+    font-size: 52px;
+    font-weight: bold;
+    padding: 30px 0 50px 50px;
+`;
+
+export const CronoContainer = styled.div`
+    font-size: 24px;
+    color: #121212;
+    display: flex;
+    flex-direction: row;
+    width: 60vw;
+    justify-content: space-between;
+    margin-top: 5vh;
+`;
+
+export const CronoContainerContainer = styled.div`
+    width: 25vw;
+    justify-content: space-between;
+`;
+
+export const CronoCard = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+export const CronoCardContent = styled.div`
+    background-color: #e5e5e5e5;
+    font-size: 18px;
+    border-radius: 8px;
+    height: 12vh;
+`;
+
+export const CronoDate = styled.div`
+    background-color: #CF2E2E;
+    padding: 15px;
+    color: #fff;
+    font-weight: 500;
+    width: 60px;
+    justify-content: center;
+    align-items: center;
+    border-top-left-radius: 8px;
+    border-bottom-left-radius: 8px;
+    height: 12vh;
+`;
+
+export const Acervo = styled.div`
+    height: 50vh;
+    background-color: #CF2E2E;
+`;
+
+export const Veteranos = styled.div`
+    height: 50vh;
+`;
+
+export const Footer = styled.div`
+    height: 50vh;
+    background-color: #eeeeee;
+`;
+
+export const FooterBottom = styled.div`
+    height: 10vh;
+    background-color: #CF2E2E;
+`;
