fac4c708c2b31f9cbcfa3c643bb57e837441c0ad
Fernnandoo <116328071+Fernnandoo@users.noreply.github.com>
2023-09-21 11:38:57 -0300

    Update Data.tsx

--- src/Data.tsx
+++ src/Data.tsx
@@ -4,56 +4,244 @@ import { IMessage } from "minimal-chatbot";
 const messages: IMessage[] = [
   {
     id: "about",
-    body: "I am a chatbot, I am here to help you.",
+    body: "O Connect Vestibulinho é uma plataforma que visa informar os potenciais alunos",
     sender: "bot",
     options: [
       {
-        body: "your services",
+        body: "Sobre a Windowally",
         nextMessageId: "services"
       },
       {
-        body: "thanks",
+        body: "Sobre você",
+        nextMessageId: "aboutBot"
+      },
+      {
+        body: "Obrigado",
         nextMessageId: "thanks"
       },
 
     ]
   },
-
   {
-    id: "services",
-    body: <p>You can access our page in <a href="https://google.com">this link</a> for more informations about our services</p>,
+    id: "other",
+    body: "Claro, o que você deseja saber?",
     sender: "bot",
     options: [
       {
-        body: "about you",
+        body: "Sobre os Blogs",
+        nextMessageId: "blog"
+      },
+      {
+        body: "Sobre o cronograma",
+        nextMessageId: "crono"
+      },
+      {
+        body: "Sobre as provas",
+        nextMessageId: "prova"
+      },
+      {
+        body: "Sobre os cursos",
+        nextMessageId: "curso"
+      },
+      {
+        body: "Sobre a galeria",
+        nextMessageId: "galeria"
+      },
+    ]
+  },
+  {
+    id: "blog",
+    body: "Em Blog, você confere o que a ETEC posta de uma forma divertida",
+    sender: "bot",
+    options: [
+      {
+        body: "Sobre o cronograma",
+        nextMessageId: "crono"
+      },
+      {
+        body: "Sobre as provas",
+        nextMessageId: "prova"
+      },
+      {
+        body: "Sobre os cursos",
+        nextMessageId: "curso"
+      },
+      {
+        body: "Sobre a galeria",
+        nextMessageId: "galeria"
+      },
+      {
+        body: "Obrigado",
+        nextMessageId: 'thanks'
+      },
+    ]
+  },
+  {
+    id: "crono",
+    body: "Em Cronograma, você pode conferir as datas relacionadas ao Vestibulinho, fica ligado!",
+    sender: "bot",
+    options: [
+      {
+        body: "Sobre os Blogs",
+        nextMessageId: "blog"
+      },
+      {
+        body: "Sobre as provas",
+        nextMessageId: "prova"
+      },
+      {
+        body: "Sobre os cursos",
+        nextMessageId: "curso"
+      },
+      {
+        body: "Sobre a galeria",
+        nextMessageId: "galeria"
+      },
+      {
+        body: "Obrigado",
+        nextMessageId: 'thanks'
+      },
+    ]
+  },
+  {
+    id: "prova",
+    body: "Em Provas, você pode conferir as provas anteriores pra se preparar pro Vestibulinho",
+    sender: "bot",
+    options: [
+      {
+        body: "Sobre os Blogs",
+        nextMessageId: "blog"
+      },
+      {
+        body: "Sobre o cronograma",
+        nextMessageId: "crno"
+      },
+      {
+        body: "Sobre os cursos",
+        nextMessageId: "curso"
+      },
+      {
+        body: "Sobre a galeria",
+        nextMessageId: "galeria"
+      },
+      {
+        body: "Obrigado",
+        nextMessageId: 'thanks'
+      },
+    ]
+  },
+  {
+    id: "curso",
+    body: "Em Cursos, você pode aprender mais sobre cada curso",
+    sender: "bot",
+    options: [
+      {
+        body: "Sobre os Blogs",
+        nextMessageId: "blog"
+      },
+      {
+        body: "Sobre o cronograma",
+        nextMessageId: "crono"
+      },
+      {
+        body: "Sobre as provas",
+        nextMessageId: "prova"
+      },
+      {
+        body: "Sobre a galeria",
+        nextMessageId: "galeria"
+      },
+      {
+        body: "Obrigado",
+        nextMessageId: 'thanks'
+      },
+    ]
+  },
+  {
+    id: "galeria",
+    body: "Em Galeria, você fica por dentro do que tem de melhor na ETEC",
+    sender: "bot",
+    options: [
+      {
+        body: "Sobre os Blogs",
+        nextMessageId: "blog"
+      },
+      {
+        body: "Sobre o cronograma",
+        nextMessageId: "crono"
+      },
+      {
+        body: "Sobre as provas",
+        nextMessageId: "prova"
+      },
+      {
+        body: "Sobre os cursos",
+        nextMessageId: "curso"
+      },
+      {
+        body: "Obrigado",
+        nextMessageId: 'thanks'
+      },
+    ]
+  },
+  {
+    id: "team",
+    body: <p>A Equipe Windowally é formada por alunos capacitados com o objetivo de divulgar a ETEC Adolpho Berezin</p>,
+    sender: "bot",
+    options: [
+      {
+        body: "Sobre você",
         nextMessageId: "about"
       },
       {
-        body: "thanks",
+        body: "Sobre você",
+        nextMessageId: "aboutBot"
+      },
+      {
+        body: "Obrigado",
         nextMessageId: "thanks"
       },
+    ]
+  },
+  {
+    id: "aboutBot",
+    body: "Eu sou o Adolphinho, o robô criado pela Windowally para ajudar",
+    sender: "bot",
+    options: [
+      {
+        body: "Obrigado",
+        nextMessageId: "thanks"
+      }
     ]
   },
 
   {
     id: "thanks",
-    body: "Thanks for the interest!",
+    body: "Espero ter respondido todas as suas dúvidas",
     sender: "bot"
   }
 ]
 
 export const firstMessage: IMessage = {
   id: "introduction",
-  body: "Hello, how can I help you?",
+  body: "Olá, como posso ajudar?",
   sender: "bot",
   options: [
     {
-      body: "about you",
+      body: "Sobre o site",
       nextMessageId: "about"
     },
     {
-      body: "your services",
-      nextMessageId: "services"
+      body: "Sobre a Windowally",
+      nextMessageId: "team"
+    },
+    {
+      body: "Sobre você",
+      nextMessageId: "aboutBot"
+    },
+    {
+      body: "Outro",
+      nextMessageId: "other"
     },
   ]
 }
