bce11d451c9010fdf449bd232e36ba04be5c0316
Fernnandoo <116328071+Fernnandoo@users.noreply.github.com>
2023-09-21 11:41:36 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Container, MainContainer } from '@/styles/global'
 import { Outlet } from 'react-router-dom'
 import Header from './interfaces/Header'
 import Footer from './interfaces/Footer'
+import ChatButton from './interfaces/ChatButton/ChatButton'
 
 
 function App() {
   return (
     <MainContainer>
+      <ChatButton />
       <Header />
       <Container>
         <Outlet />
