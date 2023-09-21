d9110a6b12c7b8c0b25b539366367f405d3e7f01
Fernnandoo <116328071+Fernnandoo@users.noreply.github.com>
2023-09-20 21:52:04 -0300

    Update Footer.tsx

--- src/interfaces/Footer/Footer.tsx
+++ src/interfaces/Footer/Footer.tsx
@@ -4,8 +4,33 @@ import * as S from "./footer.styled";
 const Footer = () => {
   return (
     <>
-      <S.Footer></S.Footer>
-      <S.FooterBottom></S.FooterBottom>
+      <S.Footer>
+        <S.Container>
+          <S.Content>
+            <S.Title>Sociais</S.Title>
+            <S.Item>Projeto</S.Item>
+            <S.Item>ETEC Adolpho Berezin</S.Item>
+            <S.Item>CPS</S.Item>
+          </S.Content>
+          <S.Content>
+            <S.Title>Contato da ETEC</S.Title>
+            <S.Item href="mailto: e107acad@cps.sp.gov.br">emailtec@email.com</S.Item>
+            <S.Item>(13) 3448-3800</S.Item>
+          </S.Content>
+          <S.Content>
+            <S.Title>Windowally</S.Title>
+            <S.Item href="mailto: windowally.contact@gmail.com">windowally.contact@gmail.com</S.Item>
+            <S.Item href="https://l.instagram.com/?u=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D61551492807955%26mibextid%3DZbWKwL&e=AT26nZhZiAIrSI445Sd1E-rVNPB8BFCPxRjdHb36wxxAcl8_p0kFkk7Qtin0PlONAs-5g3ZO23SuKWn1gzaje9i1cNRF74nRfzOubFuAy3Rf37cg"
+            >Facebook</S.Item>
+            <S.Item href="https://www.instagram.com/windowally/?igshid=OGQ5ZDc2ODk2ZA%3D%3D">Instagram</S.Item>
+          </S.Content>
+        </S.Container>
+      </S.Footer>
+      <S.FooterBottom>
+        <S.Copy>
+          © Todos direitos reservados
+        </S.Copy>
+      </S.FooterBottom>
     </>
   );
 };
