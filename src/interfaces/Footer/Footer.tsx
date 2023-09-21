e3db90fa6b95d4932882d3d8519a3060f547366e
leeool <lgonsalez02@gmail.com>
2023-09-20 23:15:52 -0300

    fix: facebook link

--- src/interfaces/Footer/Footer.tsx
+++ src/interfaces/Footer/Footer.tsx
@@ -22,7 +22,7 @@ const Footer = () => {
           <S.Content>
             <S.Title>Windowally</S.Title>
             <S.Item href="mailto: windowally.contact@gmail.com">windowally.contact@gmail.com</S.Item>
-            <S.Item href="https://l.instagram.com/?u=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D61551492807955%26mibextid%3DZbWKwL&e=AT26nZhZiAIrSI445Sd1E-rVNPB8BFCPxRjdHb36wxxAcl8_p0kFkk7Qtin0PlONAs-5g3ZO23SuKWn1gzaje9i1cNRF74nRfzOubFuAy3Rf37cg"
+            <S.Item href="https://www.facebook.com/profile.php?id=61551492807955&mibextid=ZbWKwL"
             >Facebook</S.Item>
             <S.Item href="https://www.instagram.com/windowally/?igshid=OGQ5ZDc2ODk2ZA%3D%3D">Instagram</S.Item>
           </S.Content>
